de15d2c80278b5dd8dd9b44b542f682a3d1d7059
Supermortal <christopherbyronpink@yahoo.com>
2019-02-22 12:47:05 -0700

    Make some progress with hooks conversion

--- src/NestedAccordionHooks.jsx
+++ src/NestedAccordionHooks.jsx
@@ -10,7 +10,7 @@ export const prepElementArrays = (itemElements, contents, level) => {
     };
 };
 
-export const createItemElement = (className, item, index, level, content, childItemElements, isActive = false) => {
+export const createItemElement = (stateObject, className, getItems, setChildren, item, index, level, content, childItemElements, isActive = false) => {
 
     const accordionItemContentClasses = classNames("accordion-item-content", {
         active: isActive
@@ -19,7 +19,7 @@ export const createItemElement = (className, item, index, level, content, childI
     const itemElement = (
         <li className="accordion-item" key={index}>
             <div className={accordionItemContentClasses}
-            // onClick={onItemClick(item, index, level)}
+            onClick={onItemClick(item, index, level, stateObject, getItems, setChildren)}
             >
                 {content}
             </div>
@@ -30,13 +30,13 @@ export const createItemElement = (className, item, index, level, content, childI
     return itemElement;
 };
 
-export const createItemElements = (className, items, itemElements, contents, level, getItemContent) => {
+export const createItemElements = (stateObject, className, getItems, setChildren, items, itemElements, contents, level, getItemContent) => {
 
     for (let i = 0; i < items.length; i++) {
 
         const item = items[i];
         const content = getItemContent(item);
-        const itemElement = createItemElement(className, item, i, level, content);
+        const itemElement = createItemElement(stateObject, className, getItems, setChildren, item, i, level, content);
 
         itemElements[level][i] = itemElement;
         contents[level][i] = content;
@@ -48,7 +48,7 @@ export const createItemElements = (className, items, itemElements, contents, lev
     };
 };
 
-export const initialGetItemsProcessing = (className, items, stateObject, getItemContent) => {
+export const initialGetItemsProcessing = (className, items, stateObject, getItemContent, getItems, setChildren) => {
 
     const itemElementsArgument = stateObject.itemElements;
     const contentsArgument = stateObject.contents;
@@ -61,13 +61,166 @@ export const initialGetItemsProcessing = (className, items, stateObject, getItem
     const {
         itemElements,
         contents
-    } = createItemElements(className, items, updateObject.itemElements, updateObject.contents, level, getItemContent);
+    } = createItemElements(updateObject, className, getItems, setChildren, items, updateObject.itemElements, updateObject.contents, level, getItemContent);
 
     return { itemElements, contents, selectedIndicies, storedItems };
 }
 
+export const isSecondClickFunc = (selectedIndicies, index, level) => {
+    const oldLevelIndex = selectedIndicies[level];
+    const isSecondClick = oldLevelIndex === index && level === (selectedIndicies.length - 1);
+
+    return isSecondClick;
+}
+
+export const handleSecondClick = (selectedIndicies, index, level, onChange) => {
+
+    let hasSecondClick = false;
+
+    const isSecondClick = isSecondClickFunc(selectedIndicies, index, level);
+
+    if (isSecondClick) {
+        clearAll();
+        if (onChange) onChange(null);
+        hasSecondClick = true;
+    }
+
+    return hasSecondClick;
+}
+
+export const clearAll = (getItems, initialGetItemsProcessing, setChildren, stateObject) => {
+    (new Promise((resolve, reject) => {
+        getItems(null, resolve, reject);
+    }))
+        .then(items => {
+
+            const updateObject = initialGetItemsProcessing(items, stateObject);
+            setChildren(updateObject);
+        });
+}
+
+export const setSelectedIndex = (selectedIndicies, index, level) => {
+    selectedIndicies[level] = index;
+    selectedIndicies = cleanUpArray(selectedIndicies, level);
+    return selectedIndicies;
+}
+
+export const setStoredItem = (storedItems, item, level) => {
+    storedItems[level] = item;
+    storedItems = cleanUpArray(storedItems, level);
+    return storedItems;
+}
+
+export const cleanUpArray = (array, level) => {
+    const levelPlus = level + 1;
+    array.splice(levelPlus, array.length - levelPlus);
+    return array;
+}
+
+export const replaceOldElements = (selectedIndicies, storedItems, itemElements, contents, createItemElement) => {
+
+    for (let level = 0; level < selectedIndicies.length; level++) {
+
+        const selectedIndex = selectedIndicies[level];
+        const storedItem = storedItems[level];
+        const selectedContent = contents[level][selectedIndex];
+        const childItemElements = (itemElements[level + 1]) ? itemElements[level + 1] : null;
+        const isActive = (level === selectedIndicies.length - 1) ? true : false;
+        const replacementItemElement = createItemElement(storedItem, selectedIndex, level, selectedContent, childItemElements, isActive);
+
+        itemElements[level][selectedIndex] = replacementItemElement;
+    }
+    return itemElements;
+}
+
+export const cleanUpOldItemElements = (itemElements, contents, storedItems, selectedIndicies, level) => {
+
+    itemElements = cleanUpArray(itemElements, level);
+    contents = cleanUpArray(contents, level);
+
+    const selectedIndex = selectedIndicies[level];
+    const storedItem = storedItems[level];
+    const selectedContent = contents[level][selectedIndex];
+    const replacementItemElement = createItemElement(storedItem, selectedIndex, level, selectedContent);
+
+    itemElements[level][selectedIndex] = replacementItemElement;
+
+    return {
+        itemElements,
+        contents
+    };
+}
+
+export const onItemClickPreprocess = (item, index, level, stateObject, onChange) => {
+
+    const argumentSelectedIndicies = (stateObject.selectedIndicies) ? stateObject.selectedIndicies : [];
+    const argumentItemElements = stateObject.itemElements;
+    const argumentContents = stateObject.contents;
+    const argumentStoredItems = (stateObject.storedItems) ? stateObject.storedItems : [];
+
+    const hasSecondClick = handleSecondClick(argumentSelectedIndicies, index, level, onChange);
+    if (hasSecondClick) return null;
+
+    if (onChange) onChange(item);
+
+    const {
+        itemElements,
+        contents
+    } = cleanUpOldItemElements(argumentItemElements,
+        argumentContents, argumentStoredItems, argumentSelectedIndicies, level);
+
+    const selectedIndicies = setSelectedIndex(argumentSelectedIndicies, index, level);
+    const storedItems = setStoredItem(argumentStoredItems, item, level);
+
+    return { itemElements, contents, selectedIndicies, storedItems };
+}
+
+export const onItemClickPostprocess = (level, items, stateObject, prepElementArrays, createItemElements, replaceOldElements) => {
+
+    const itemElementsArgument = stateObject.itemElements;
+    const contentsArgument = stateObject.contents;
+    const selectedIndicies = stateObject.selectedIndicies;
+    const storedItems = stateObject.storedItems;
+
+    const newLevel = level + 1;
+
+    let updateObject = prepElementArrays(itemElementsArgument, contentsArgument, newLevel);
+    updateObject = createItemElements(items, updateObject.itemElements, updateObject.contents, newLevel);
+    let itemElements = updateObject.itemElements;
+    const contents = updateObject.contents;
+
+    itemElements = replaceOldElements(selectedIndicies, storedItems, itemElements, contents);
+
+    return {
+        itemElements,
+        contents,
+        selectedIndicies,
+        storedItems
+    };
+}
+
+export const onItemClick = (item, index, level, stateObject, setChildren, getItems) => {
+    return (e) => {
+
+        let updateObject = onItemClickPreprocess(item, index, level, stateObject);
+        if (!updateObject) return;
+
+        setChildren(updateObject);
+
+        (new Promise((resolve, reject) => {
+            getItems(item, resolve, reject);
+        }))
+            .then((items) => {
+                if (!items) return;
+
+                updateObject = onItemClickPostprocess(level, items, updateObject);
+                setChildren(updateObject);
+            });
+    };
+}
+
 export default function NestedAccordion(props) {
-    const [{ itemElements, contents, selectedIndicies, storedItems }, setChildren] = useState(() => {
+    const [storedState, setChildren] = useState(() => {
 
         let initialState = {
             itemElements: [],
@@ -91,8 +244,10 @@ export default function NestedAccordion(props) {
                 const updateObject = initialGetItemsProcessing(
                     props.className,
                     items,
-                    { itemElements, contents, selectedIndicies, storedItems },
-                    props.getItemContent);
+                    storedState,
+                    props.getItemContent,
+                    props.getItems,
+                    setChildren);
                 setChildren(updateObject);
             });
 
@@ -101,7 +256,7 @@ export default function NestedAccordion(props) {
 
     return (
         <ul className={(props.className) ? props.className : "nested-accordion"}>
-            {itemElements[0]}
+            {storedState.itemElements[0]}
         </ul>
     );
 }
